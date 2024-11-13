f47d70169a35baba6feed57fc1e4449ed23cb343
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2024-11-13 17:19:21 +0530

    Completed registration part

--- home/views.py
+++ home/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Posts
 # Create your views here.
-
-posts=[
- {
-    'title' : 'blog1',
-    'author' : 'a1',
-    'content' : 'aaaaaa',
-    'date_posted' : datetime(2024,11,1)
-  },
- {
-    'title' : 'blog2',
-    'author':'a2',
-    'content' : 'bbbb',
-    'date_posted' : datetime(2024, 10, 4)
-}
- ]
 
 
 def index(request):
     context={
-    'posts' : posts
+    'posts' : Posts.objects.all()
     }
     return render(request,'home/index.html',context)
 
