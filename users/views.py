a1a97f903ef86415b1816324b4f8f766a1afad97
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2024-11-15 01:17:22 +0530

    Added user login functionality

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 
 def register(request):
@@ -9,10 +11,20 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account is created {username}!')
-            return redirect('home-index')
+            messages.success(request,f'Your account has been created successfully. You can login now.')
+            return redirect('login')
         else:
             messages.error(request, "There was an error in your registration form.")
     else:
         form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
+
+
+def custom_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html')
+
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
