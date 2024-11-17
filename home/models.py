53e6d7d85b11e53d97d1cff2b019fe8a9fd0132f
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2024-11-18 00:39:23 +0530

    Completed Blog Crud using class based views

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
 
 class Posts(models.Model):
@@ -11,4 +12,8 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
 
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='home-index'),
-    path('about/', views.about, name='home-about'),
+    path('', PostListView.as_view(), name='home-index'),
+    path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
+    path('post/new/',PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'), 
+    path('about/', about, name='home-about'),
 ]
