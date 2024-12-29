cce358d729d31ce361ca7ba18e401c632e4149db
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2024-12-29 12:32:31 +0530

    Added icons for upvotes and thoughts

--- users/models.py
+++ users/models.py
@@ -8,6 +8,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = CloudinaryField('image',default='default_tkihgy')
     bio = models.TextField(blank=True,null=True)
+    
+    def subscribers_count(self):
+        from interactions.models import Subscription
+        return Subscription.objects.filter(subscribed_to=self.user).count()
 
     def __str__(self):
         return f'{self.user.username} Profile'
